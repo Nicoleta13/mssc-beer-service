ec95b0463a7129d38182a2058277f1ab2a5b3cdf
Nicoleta <nicoleta.platniuc@gmail.com>
2021-03-30 12:23:06 -0500

    adding controller tests Closes #4

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,13 +1,55 @@
 package guru.springframework.msscbeerservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbeerservice.web.controller.BeerController;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@SpringBootTest
+@WebMvcTest(BeerController.class)
 class MsscBeerServiceApplicationTests {
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
-    void contextLoads() {
+    void getBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"+ UUID.randomUUID().toString())
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() throws Exception  {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(post("/api/v1/beer/").contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(put("/api/v1/beer/"+ UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isNoContent());
+
     }
 
 }
